7f4f19b6c60ca8d2b88482317e3541320d44936c
Itai Tenenbaum <itai@Titan.local>
2016-03-26 19:23:12 +0300

    main methods

--- playground/main.js
+++ playground/main.js
@@ -1,15 +1,40 @@
 // AjaxServant
 
-function run () {
-	let test = checkExternalAPI();
-
-	alert(test);
+function log (title, result) {
+	console.log(title, result);
 }
 
-function checkExternalAPI () {
+function runTests () {
+	log('running tests...', '');
+
+	testMain();
+	testApi();
+}
+
+function testMain () {
 	if (typeof AjaxServant !== 'function') return false;
 
-	const instance = new AjaxServant();
-	
-	return typeof instance.xhr === 'object';
+	const instance = createSimple();
+
+	log('whois', instance.whois === 'AjaxServant')
+}
+
+function testApi () {
+	const servant = createSimple();
+
+	log('has prop .xhr', typeof servant.xhr === 'object' && servant.xhr instanceof XMLHttpRequest)
+	log('has prop .verb', servant.verb === 'GET');
+	log('has prop .url', servant.url === '/');
+	log('has prop .async', servant.async === true);
+	log('has method .config()', typeof servant.config === 'function');
+	log('has method .open()', typeof servant.open === 'function');
+	log('has method .setHeaders()', typeof servant.setHeaders === 'function');
+	log('has method .setHeader()', typeof servant.setHeader === 'function');
+	log('has method .send()', typeof servant.send === 'function');
+	log('has method .abort()', typeof servant.abort === 'function');
+	log('has method .go()', typeof servant.go === 'function');
+}
+
+function createSimple() {
+	return new AjaxServant();
 }
--- src.js
+++ src.js
@@ -1,19 +1,78 @@
 var AjaxServant = (function (win, doc) {
 	// private funcs
+	function forIn (obj, cbFn) {
+		var key;
+		var hasOwn = Object.hasOwnProperty;
+
+		for (key in obj) { if (hasOwn.call(obj, key)) {
+			cbFn.call(obj, key, obj[key]);
+		}}
+	}
+
+	function mixin(target, ...sources) {
+		sources.forEach(srcObj => {
+			forIn(srcObj, (key, value) => {
+				target[key] = value;
+			});
+		});
+
+		return target;
+	}
+
 	function createXHR () {
 		return new XMLHttpRequest();
 	}
 
+	const defaultOptions = {
+		async: true
+	};
+
 	// constructor
-	var AjaxServant = function (verb, url) {
+	var AjaxServant = function (verb = 'GET', url = '/', options = defaultOptions) {
+		this.events = {};
 		this.xhr = createXHR();
+		this.config(verb, url, options);
 	}
 
 	// constructor prototype
 	var proto = {
-		constructor: AjaxServant
+		whois: 'AjaxServant',
+		constructor: AjaxServant,
+		config: function (verb = 'GET', url = '/', options = defaultOptions) {
+			this.verb  = verb;
+			this.url   = url;
+			this.async = (typeof options.async === 'undefined') ? true : options.async;
+		},
+		open: function () {
+			this.xhr.open(this.verb, this.url, this.async);
+		},
+		setHeaders: function () {
+			if (!this.headers || typeof this.headers !== 'object') {return null;}
+
+			const xhr = this.xhr;
+
+			forIn(this.headers, function (key, value) {
+				xhr.setRequestHeader(key, value);
+			});
+		},
+		setHeader: function (key, value) {
+			xhr.setRequestHeader(key, value);
+		},
+		send: function (urlParams, data) {
+			data = JSON.stringify(data);
+			this.xhr.send(data || null);
+		},
+		abort: function () {
+			this.xhr.abort();
+		},
+		go () {
+			this.open();
+			this.setHeaders();
+			this.send();
+		}
 	};
 
+	AjaxServant.prototype = proto;
 
 	return AjaxServant;
 
