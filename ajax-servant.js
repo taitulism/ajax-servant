44736f0c5f47a98491b0fe39ae3622c98e9c8798
Itai Tenenbaum <itai@Titan.local>
2016-03-27 14:22:17 +0300

    abort() dismiss() & all proto-methods return this

--- ajax-servant.js
+++ ajax-servant.js
@@ -42,39 +42,76 @@ var AjaxServant = (function (win, doc) {
 			this.verb  = verb;
 			this.url   = url;
 			this.async = (typeof options.async === 'undefined') ? true : options.async;
+
+			return this;
 		},
 		on (eventName, cbFn) {
+			this.xhr = this.xhr || createXHR();
 
+			return this;
 		},
 		open () {
+			this.xhr = this.xhr || createXHR();
+
 			this.xhr.open(this.verb, this.url, this.async);
+
+			return this;
 		},
 		setHeaders () {
 			if (!this.headers || typeof this.headers !== 'object') {return null;}
+
+			this.xhr = this.xhr || createXHR();
 
 			const xhr = this.xhr;
 
 			forIn(this.headers, function (key, value) {
 				xhr.setRequestHeader(key, value);
 			});
+
+			return this;
 		},
 		setHeader (key, value) {
+			this.xhr = this.xhr || createXHR();
+
 			xhr.setRequestHeader(key, value);
+
+			return this;
 		},
 		send (urlParams, data) {
+			this.xhr = this.xhr || createXHR();
+
 			data = JSON.stringify(data);
 			this.xhr.send(data || null);
+
+			return this;
 		},
 		abort () {
-			this.xhr.abort();
+			this.xhr && this.xhr.abort();
+
+			return this;
 		},
 		go () {
+			this.xhr = this.xhr || createXHR();
+
 			this.open();
 			this.setHeaders();
 			this.send();
+
+			return this;
 		},
-		die () {
+		dismiss () {
+			var xhr = this.xhr;
+
+			this.abort();
+
 			// remove listeners
+			forIn(this.events, function (eventName, handler) {
+				xhr.removeListener(eventName, handler);
+			});
+
+			this.xhr = null;
+
+			return this;
 		}
 	};
 
