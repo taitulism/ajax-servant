d2c53896af2f2dc8f6b3e8022a88e01dc6c0527e
Taitu Lizenbaum <taitulism@gmail.com>
2016-04-13 22:31:49 +0300

    future tests

--- test/index.js
+++ test/index.js
@@ -32,6 +32,11 @@ describe('AjaxServant', function() {
 	describe('instance', function () {
 		const CONSTRUCTOR_ERROR = 'AjaxServant requires two strings';
 
+		it('should have no events attached when created', function () {
+			const servant = new AjaxServant('GET', '/');
+			// expect(servant).to.have.property('on');
+		});
+
 		it('should have a .on() API method', function () {
 			const servant = new AjaxServant('GET', '/');
 			expect(servant).to.have.property('on');
@@ -89,8 +94,115 @@ describe('AjaxServant', function() {
 
 				servant.on('response', handler);
 
-				console.log(servant.events)
+				console.log(1, servant.events)
+			});
+
+			it('should add an event handler with context', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should add only one native(!) event handler', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should throw an error when called with invalid arguments', function () {
+				expect(true).to.be(false);
+			});
+		});
+
+		describe('.send()', function () {
+			it('should add an event handler', function () {
+				const servant = new AjaxServant('GET', '/api');
+				const handler = function handler () {};
+
+				servant.send();
+
+				console.log(1, servant.events)
+			});
+
+			it('should add an event handler with context', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should send body to the server (same domain)', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should send queryString to the server (same domain)', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should send headers to the server (same domain)', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should send URL params to the server (same domain)', function () {
+				expect(true).to.be(false);
+			});
+		});
+
+		describe('.abort()', function () {
+			it('should add an event handler', function () {
+				const servant = new AjaxServant('GET', '/api');
+				const handler = function handler () {};
+
+				servant.on('response', handler);
+				servant.send();
+				servant.abort('response', handler);
+
+				console.log(1, servant.events)
+			});
+
+			it('should add an event handler with context', function () {
+				expect(true).to.be(false);
+			});
+		});
+
+		describe('.dismiss()', function () {
+			it('should add an event handler', function () {
+				const servant = new AjaxServant('GET', '/api');
+				const handler = function handler () {};
+
+				servant.dismiss('response', handler);
+
+				console.log(1, servant.events)
+			});
+
+			it('should abort a running server', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should unbind all event handlers', function () {
+				expect(true).to.be(false);
+			});
+
+			it('should delete the servant\'s XHR', function () {
+				expect(true).to.be(false);
 			});
 		});
 	})
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
